2476d64d1cbc75d69f441aedaf361ffb0610fb51
Peter Gao <peter_gao@outlook.com>
2023-10-14 09:05:39 -0700

    change to browserRouter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import React from 'react'
 
 
-import { Routes, Route, HashRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import About from './Pages/About';
 import ArticlePage from './Pages/ArticlePage'
 import ArticleList from './Pages/ArticleList'
@@ -13,7 +13,7 @@ import NotFoundPage from './Pages/NotFoundPage';
 
 function App() {
     return (
-        <HashRouter>
+        <BrowserRouter>
             <div className="App">
                 <NavBar />
                 <div id='app-body'>
@@ -26,8 +26,7 @@ function App() {
                     </Routes>
                 </div>
             </div>
-        </HashRouter>
-
+        </BrowserRouter>
     );
 }
 export default App;
